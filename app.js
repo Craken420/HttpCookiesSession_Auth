a7dcbaeadc6bacdb5adfaa08c36d87d6ed7356b3
Luis Angel Peña <lapena@mavi.mx>
2021-04-16 12:36:58 -0500

    feat:: Add an error handler function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Dependencies
 var express = require('express'),
+    createError = require('http-errors'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     cookie = require('cookie'), // Analizador de cookies
@@ -302,5 +303,22 @@ sessionRouter.get('/logout', (req, res) => {
 
 app.use('/session', sessionRouter)
 app.use('/cookies', cookiesRouter)
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
